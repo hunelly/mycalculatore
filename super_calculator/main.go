298052e5dae02c001994a79711ceb7db1b1d7f8f
hunelly <100610189+hunelly@users.noreply.github.com>
2023-05-14 13:32:51 +0200

    Update main.go

--- super_calculator/main.go
+++ super_calculator/main.go
@@ -3,13 +3,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import (
-"os"
-
-_ "embed"
-"github.com/goreleaser/goreleaser/cmd"
-"super_calculator/cmd"
-)
+import "super_calculator/cmd"
 
 func main() {
 	cmd.Execute()
